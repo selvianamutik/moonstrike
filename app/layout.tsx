3201c0aba126202d3927c401195d1add0e8b4fbf
KhoirudinWidiyansa <kwidiyansa@gmail.com>
2026-09-14 02:08:01 +0700

    fix: update Facebook Pixel ID for tracking consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -178,7 +178,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '38272410049038920');
+            fbq('init', '4441562369450646');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -187,7 +187,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=38272410049038920&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=4441562369450646&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
